5bd4405c166512a391a8db250553182aa51a9954
lepoint2023-hue <lepoint2023@gmail.com>
2026-04-21 09:27:12 +0200

    Update module6-api.js

--- module6-api.js
+++ module6-api.js
@@ -16,7 +16,7 @@
 const API_KEY = "AIzaSyAAmbvU5jPFiB8ugsPuvYoqBxvMU76UFLw";
 
 const API_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
   API_KEY;
 
 /* ── Constantes ── */
